6dc1cefcac4caa9d26c69518e0863fc80a44e4c3
jinjun <757258777@qq.com>
2020-03-27 10:14:31 +0800

    加入单元测试

--- tests/TestOrder.php
+++ tests/TestOrder.php
@@ -1,14 +1,16 @@
 <?php
 namespace Cblink\MeituanDispatch\Tests;
 
+use Cblink\MeituanDispatch\MeituanDispatchException;
+use phpDocumentor\Reflection\Types\Array_;
 use PHPUnit\Framework\TestCase;
 use Cblink\MeituanDispatch\Dispatch;
 
 class TestOrder extends TestCase
 {
-    protected $secret = '';
+    protected $secret = ';zU7^Tb]Ul#}_e.*uJ4o*^V6Dp]y<=)qT[:*:40M,Hw.CR!]s5iM.SyL_au]BOL8';
 
-    protected $appKey = '';
+    protected $appKey = '78395bb1c91048d2b6f6a762352ab89a';
 
     /**
      * 创建订单
@@ -28,9 +30,9 @@ class TestOrder extends TestCase
             'poi_seq' => '40001', // 美团的流水号加4为前缀
             'shop_id' => 'test_0001',
             'delivery_service_code' => 4011,
-            'receiver_name' => '测试',
-            'receiver_address' => '长虹大厦',
-            'receiver_phone' => '13944700000',
+            'receiver_name' => 'test',
+            'receiver_address' => '中国',
+            'receiver_phone' => '13900000000',
             'receiver_lng' => (int) (113.957613 * pow(10, 6)),
             'receiver_lat' => (int) (22.538135 * pow(10, 6)),
             'goods_value' => 100,
@@ -39,6 +41,26 @@ class TestOrder extends TestCase
         ];
 
         $result = (new Dispatch(['app_key' => $this->appKey, 'secret' => $this->secret, 'debug' => false]))->createByShop($data);
+
+        $this->assertArrayHasKey('data', $result);
+    }
+
+    /**
+     * 查询订单状态
+     *
+     * @return mixed
+     * @throws MeituanDispatchException
+     */
+    public function testQueryStatus()
+    {
+        $result = (new Dispatch([
+            'app_key' => $this->appKey,
+            'secret' => $this->secret,
+            'debug' => false
+        ]))->queryStatus([
+            'mt_peisong_id' => '1585274485081001060',
+            'delivery_id' => '1'
+        ]);
 
         $this->assertArrayHasKey('data', $result);
     }
